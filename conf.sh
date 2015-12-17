bb2aef18e9ea2794f4d7c9c999407b50369f5d9b
Valentin N <valenti.nikolaev@gmail.com>
2015-12-18 00:21:19 +0200

    % conf.sh. Cosmetic startBashCommand, getProcessCounter,killProcess

--- conf.sh
+++ conf.sh
@@ -31,9 +31,10 @@ checkSudo() {
   if [[ $(id -u) == 0 ]]; then
     if [[ -z "$SUDO_COMMAND" ]]; then
       msg 'Please, use sudo'
-        exit 1
-      fi
-  else  msg 'Please, use sudo or login as root'
+      exit 1
+    fi
+  else
+    msg 'Please, use sudo or login as root'
     exit 1
   fi
 }
@@ -73,61 +74,84 @@ checkScriptParams() {
   fi
 }
 
+#######################################
+# Start worker by it's filename
+# Globals:
+#   WORKERS_DIR_LIST
+#   LOGFILE
+#   LOGFILEERROR
+# Locals:
+#   PROCESS_NAME
+#   PROCESS_RUN
+# Arguments:
+#   worker name (process name need to start)
+# Returns:
+#   None
+#######################################
+
 startBashCommand() {
-    if [ -z "$1" ]
-    then
-        msg "Missing worker name"  # Or no parameter passed.
-        exit 1
+  if [ -z "$1" ]; then
+    msg "Missing worker name"  # Or no parameter passed.
+    exit 1
+  fi
+  local PROCESS_NAME=$1
+  local PROCESS_RUN=0
+  for WORKERS_DIR in $WORKERS_DIR_LIST; do
+    if [[ -d "$WORKERS_DIR" ]]; then
+      if [[ -f "$WORKERS_DIR/${PROCESS_NAME}" ]]; then
+        PROCESS_RUN=1;
+        msg "Running $WORKERS_DIR/${PROCESS_NAME} >> $WORKERS_DIR$LOGFILE$PROCESS_NAME 2>> $WORKERS_DIR$LOGFILEERROR$PROCESS_NAME"
+        bash "$WORKERS_DIR/${PROCESS_NAME}" >> $WORKERS_DIR$LOGFILE$PROCESS_NAME 2>> $WORKERS_DIR$LOGFILEERROR$PROCESS_NAME &
+        break;
+      fi
     fi
-    local PROCESS_NAME=$1
-    local PROCESS_RUN=0
-    for WORKERS_DIR in $WORKERS_DIR_LIST
-    do
-        if [[ -d "$WORKERS_DIR" ]]; then
-            if [[ -f "$WORKERS_DIR/${PROCESS_NAME}" ]]; then
-                PROCESS_RUN=1;
-                msg "Running $WORKERS_DIR/${PROCESS_NAME} >> $WORKERS_DIR$LOGFILE$PROCESS_NAME 2>> $WORKERS_DIR$LOGFILEERROR$PROCESS_NAME"
-                bash "$WORKERS_DIR/${PROCESS_NAME}" >> $WORKERS_DIR$LOGFILE$PROCESS_NAME 2>> $WORKERS_DIR$LOGFILEERROR$PROCESS_NAME &
-                break;
-            fi
-        fi
-    done
-    if [ ${PROCESS_RUN} -eq 0 ]; then
-        msg "$PROCESS_NAME failed to start";
-    fi
+  done
+  if [ ${PROCESS_RUN} -eq 0 ]; then
+    msg "$PROCESS_NAME failed to start";
+  fi
 }
 
+#######################################
+# Return process counter
+# Arguments:
+#   process name
+# Returns:
+#   process count
+#######################################
 getProcessCounter() {
-    if [ -z "$1" ]
-    then
-        msg "Missing process name"  # Or no parameter passed.
-        exit 1
-    fi
-    ps xao command | grep $1  | grep -v 'grep' | wc -l
-    return 1
+  if [ -z "$1" ];  then
+    msg "Missing process name"  # Or no parameter passed.
+    exit 1
+  fi
+  ps xao command | grep $1  | grep -v 'grep' | wc -l
+  return 1
 }
 
+#######################################
+# Kill process by name
+# Locals:
+#   PROCESS_COUNTER
+# Arguments:
+#   process name to kill
+# Returns:
+#   None
+#######################################
 killProcess() {
-    if [ -z "$1" ]
-    then
-        msg "Missing process name"  # Or no parameter passed.
-        exit 1
-    fi
-
-    PROCESS_COUNTER=$(getProcessCounter $1)
-    if [ -z "$PROCESS_COUNTER"  -o $PROCESS_COUNTER -eq 0 ]
-    then
-        msg "'$1' already Already killed"
-    else
-        PROCESSES=$(ps -ef | grep "$1"  | grep -v 'grep' | awk '{print $2}')
-
-        for processSID in "$PROCESSES"
-        do
-            msg "$1[pid $processSID] killing..."
-            msg "Killing pid $processSID..."
-            kill -9 $processSID
-        done
-    fi
+  if [ -z "$1" ]; then
+    msg "Missing process name"  # Or no parameter passed.
+    exit 1
+  fi
+  local PROCESS_COUNTER=$(getProcessCounter $1)
+  if [ -z "$PROCESS_COUNTER"  -o $PROCESS_COUNTER -eq 0 ]; then
+    msg "'$1' already Already killed"
+  else
+    PROCESSES=$(ps -ef | grep "$1"  | grep -v 'grep' | awk '{print $2}')
+    for processSID in "$PROCESSES"; do
+      msg "$1[pid $processSID] killing..."
+      msg "Killing pid $processSID..."
+      kill -9 $processSID
+    done
+  fi
 }
 
 
