32762de0b2395d9688d1b7f397577b1445e925c8
Valentin N <valenti.nikolaev@gmail.com>
2015-12-17 23:53:18 +0200

    % replace echo with msg

--- conf.sh
+++ conf.sh
@@ -12,35 +12,39 @@ WORKERS_DIR_LIST="/var/www/html/bash";
 # php worker names. The same formatting as WORKERS_DIR_LIST
 LIST_WORKERS="queue_workers"
 
+msg() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
+}
+
 checkSudo() {
     if [[ $(id -u) == 0 ]]; then
         if [[ -z "$SUDO_COMMAND" ]]; then
-            echo 'Please, use sudo'
+            msg 'Please, use sudo'
             exit;
         fi
-    else  echo 'Please, use sudo or login as root'
+    else  msg 'Please, use sudo or login as root'
         exit 1
     fi
 }
 
 checkScriptParams() {
     if [ -z "$WORKERS_DIR_LIST" ]; then
-       echo "Empty WORKERS_DIR_LIST";
+       msg "Empty WORKERS_DIR_LIST";
        exit 1
     fi
 
     if [ -z "$LOGFILE" ]; then
-       echo "Empty LOGFILE";
+       msg "Empty LOGFILE";
        exit 1
     fi
 
     if [ -z "$LOGFILEERROR" ]; then
-       echo "Empty LOGFILEERROR";
+       msg "Empty LOGFILEERROR";
        exit 1
     fi
 
     if [ -z "$LIST_WORKERS" ]; then
-       echo "Empty LIST_WORKERS";
+       msg "Empty LIST_WORKERS";
        exit 1
     fi
 }
@@ -48,7 +52,7 @@ checkScriptParams() {
 startBashCommand() {
     if [ -z "$1" ]
     then
-        echo "Missing worker name"  # Or no parameter passed.
+        msg "Missing worker name"  # Or no parameter passed.
         exit 1
     fi
     local PROCESS_NAME=$1
@@ -58,21 +62,21 @@ startBashCommand() {
         if [[ -d "$WORKERS_DIR" ]]; then
             if [[ -f "$WORKERS_DIR/${PROCESS_NAME}" ]]; then
                 PROCESS_RUN=1;
-                echo "Running $WORKERS_DIR/${PROCESS_NAME} >> $WORKERS_DIR$LOGFILE$PROCESS_NAME 2>> $WORKERS_DIR$LOGFILEERROR$PROCESS_NAME"
+                msg "Running $WORKERS_DIR/${PROCESS_NAME} >> $WORKERS_DIR$LOGFILE$PROCESS_NAME 2>> $WORKERS_DIR$LOGFILEERROR$PROCESS_NAME"
                 bash "$WORKERS_DIR/${PROCESS_NAME}" >> $WORKERS_DIR$LOGFILE$PROCESS_NAME 2>> $WORKERS_DIR$LOGFILEERROR$PROCESS_NAME &
                 break;
             fi
         fi
     done
     if [ ${PROCESS_RUN} -eq 0 ]; then
-        echo "$PROCESS_NAME failed to start";
+        msg "$PROCESS_NAME failed to start";
     fi
 }
 
 getProcessCounter() {
     if [ -z "$1" ]
     then
-        echo "Missing process name"  # Or no parameter passed.
+        msg "Missing process name"  # Or no parameter passed.
         exit 1
     fi
     ps xao command | grep $1  | grep -v 'grep' | wc -l
@@ -82,26 +86,24 @@ getProcessCounter() {
 killProcess() {
     if [ -z "$1" ]
     then
-        echo "Missing process name"  # Or no parameter passed.
+        msg "Missing process name"  # Or no parameter passed.
         exit 1
     fi
 
     PROCESS_COUNTER=$(getProcessCounter $1)
     if [ -z "$PROCESS_COUNTER"  -o $PROCESS_COUNTER -eq 0 ]
     then
-        echo "Already killed '$1'"
+        msg "'$1' already Already killed"
     else
         PROCESSES=$(ps -ef | grep "$1"  | grep -v 'grep' | awk '{print $2}')
 
         for processSID in "$PROCESSES"
         do
-            echo "$1[pid $processSID] killing..."
-            echo "Killing pid $processSID..."
+            msg "$1[pid $processSID] killing..."
+            msg "Killing pid $processSID..."
             kill -9 $processSID
         done
     fi
 }
 
-msg() {
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
-}
+
